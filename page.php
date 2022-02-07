d532a2924cdf86b3a2300cd25109e29e12384cbf
stefscan <charettestephane@cgmatane.qc.ca>
2022-02-07 16:58:03 -0500

    Add page template

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+<?php if(have_posts()) : ?>
+
+    <?php while(have_posts(  )) : the_post(  ); ?>
+
+        <article class="flex post">
+
+            <div class="texte-article">
+                <h2><?php the_title(  ) ?></h2>
+                <p><?php the_content(  ) ?></p>
+            </div>
+
+        </article>
+
+    <?php endwhile; ?>
+
+<?php endif; ?>
+
+<?php get_footer(  ) ?>
